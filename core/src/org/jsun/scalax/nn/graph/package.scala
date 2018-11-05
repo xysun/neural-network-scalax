db0cb49f9cdf791de64a5e399220de0fe9473fff
Xiayun Sun <xiayunsun@gmail.com>
2018-11-05 21:54:22 +0000

    unify type

--- core/src/org/jsun/scalax/nn/graph/package.scala
+++ core/src/org/jsun/scalax/nn/graph/package.scala
@@ -15,22 +15,22 @@ package object graph {
   case class MeDouble(v:Double) extends Numeric
   case class MeVector(v:Vector[Double]) extends Numeric
 
-  val Addition = State[List[Numeric], Double]{
+  val Addition = State[List[Numeric], Numeric]{
     case MeDouble(a) :: MeDouble(b) :: tail =>
       val ans = a+b
-      (MeDouble(ans) :: tail, ans)
+      (MeDouble(ans) :: tail, MeDouble(ans))
   }
 
-  val Logit = State[List[Numeric], Double]{
+  val Logit = State[List[Numeric], Numeric]{
     case MeDouble(h)::t =>
       val ans = 1 / (1 + math.pow(math.E, -h))
-      (MeDouble(ans)::t, ans)
+      (MeDouble(ans)::t, MeDouble(ans))
   }
 
-  val Matmul = State[List[Numeric], Double] {
+  val Matmul = State[List[Numeric], Numeric] {
     case MeVector(a) :: MeVector(b) :: tail =>
       val ans = a.zip(b).map{case (x,y) => x*y}.sum
-      (MeDouble(ans)::tail, ans)
+      (MeDouble(ans)::tail, MeDouble(ans))
   }
 
 
