721a8bc6f7160dea1f92254556554afbd7d979ad
Xiayun Sun <xiayunsun@gmail.com>
2018-11-07 22:00:30 +0000

    MNIST with graph!

--- core/src/org/jsun/scalax/nn/Main.scala
+++ core/src/org/jsun/scalax/nn/Main.scala
@@ -5,6 +5,7 @@ import java.nio.file.Paths
 import cats.effect.{ContextShift, IO}
 import fs2.{Chunk, io}
 import org.jsun.scalax.nn.models.{LogisticRegression, NeuralNetwork}
+import org.jsun.scalax.nn.graph.Tensor._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -16,7 +17,7 @@ object Main extends App {
 
   // read MNIST dataset
   val trainLabelFileName = "/Users/xiayunsun/Downloads/train-labels-idx1-ubyte"
-  val trainImgFileName   = "/Users/xiayunsun/Downloads/train-images-idx3-ubyte"
+  val trainImgFileName = "/Users/xiayunsun/Downloads/train-images-idx3-ubyte"
 
   val trainLabels: fs2.Stream[IO, Int] = // todo: must we use IO?
     io.file
@@ -34,23 +35,23 @@ object Main extends App {
       .map(_.toVector)
       .map(_.toMatrix2D(rows = imgDimension, cols = imgDimension))
 
-//  val trainData: fs2.Stream[IO, (Int, Matrix2D[Int])] = trainLabels.zip(trainImages)
+  //  val trainData: fs2.Stream[IO, (Int, Matrix2D[Int])] = trainLabels.zip(trainImages)
 
   // optional: sanity check
-//  Preprocessor.sanityCheck(trainLabels, trainImages)
+  //  Preprocessor.sanityCheck(trainLabels, trainImages)
 
   // proprocess: x /= 255. y: binary classifier on digit 0
   val trainImagesPreprocessed: fs2.Stream[IO, Matrix2D[Double]] = trainImages.map(_.mmap(_ / 255.0))
-  val trainLabelsPreprocessed: fs2.Stream[IO, Int]              = trainLabels.map(i => if (i == 0) 1 else 0)
+  val trainLabelsPreprocessed: fs2.Stream[IO, Int] = trainLabels.map(i => if (i == 0) 1 else 0)
 
-  val trainData:fs2.Stream[IO, (Int, Matrix2D[Double])] = trainLabelsPreprocessed.zip(trainImagesPreprocessed)
+  val trainData: fs2.Stream[IO, (Int, Matrix2D[Double])] = trainLabelsPreprocessed.zip(trainImagesPreprocessed)
 
   // shuffle training data: todo
 
   // single neuron: W = 784x1, with sigmoid as activation function
   // todo: can we get rid of var?
   val initialWeights = Vector.tabulate(784)(_ => 0.01)
-  val initialBias:Double = 0
+  val initialBias: Double = 0
 
   type Param = (Vector[Double], Double)
 
@@ -61,44 +62,93 @@ object Main extends App {
   val batchSize = 1000
 
 
-  val trainSink:(Param, Chunk[(Int, Matrix2D[Double])]) => Param = { case ((weights, bias), chk) =>
+  val trainSink: (Param, Chunk[(Int, Matrix2D[Double])]) => Param = {
+    case ((weights, bias), chk) =>
 
       val ys = chk.map(_._1)
-      val yHats = chk.map{case (y, img) => network.forwardProp(weights, bias, img)}
-      val losses = yHats.zip(ys).map{case (yHat, y) => network.loss(yHat, y)}
+      val yHats = chk.map { case (y, img) => network.forwardProp(weights, bias, img) }
+      val losses = yHats.zip(ys).map { case (yHat, y) => network.loss(yHat, y) }
 
       val avgLoss = losses.toVector.sum / chk.size
       println(s"avg loss for chunk: $avgLoss")
 
-      val gradients:Chunk[(Vector[Double], Double)] = yHats.zip(chk).map{case (yHat, (y, img)) => network.backProp(yHat, y, img)}
+      val gradients: Chunk[(Vector[Double], Double)] = yHats.zip(chk).map { case (yHat, (y, img)) => network.backProp(yHat, y, img) }
 
       val avgBiasGradient = gradients.map(_._2).toVector.sum / chk.size
 
       val sumWeightsGradient = gradients.map(_._1).toVector
-        .reduce[Vector[Double]]{case (v1,v2) => v1.zip(v2).map(t => t._1 + t._2)}
+        .reduce[Vector[Double]] { case (v1, v2) => v1.zip(v2).map(t => t._1 + t._2) }
 
       val avgWeightsGradient = sumWeightsGradient.map(_ / chk.size)
 
       (
-        weights.zip(avgWeightsGradient).map{case (w, g) => w - learningRate * g},
+        weights.zip(avgWeightsGradient).map { case (w, g) => w - learningRate * g },
         bias - learningRate * avgBiasGradient
       )
 
   }
 
+  val graph = for {
+    _ <- BinaryStateNode(CrossProduct)
+    _ <- BinaryStateNode(Add)
+    ans <- BinaryStateNode(CrossEntropyLoss)
+  } yield ans
+
+  val trainSinkWithGraph: (Param, Chunk[(Int, Matrix2D[Double])]) => Param = {
+    case ((weights, bias), chk) =>
+
+      val w = Node("w", TVector(weights), Ident)
+      val b = Node("b", Scalar(bias), Ident)
+
+      val a =
+        chk.map { case (_y, img) =>
+          val x = Node("x", TVector(img.flatten), Ident)
+          val y = Node("y", Scalar(_y), Ident)
+          val args = List(w, x, b, y)
+
+          val init = (args, emptyGraph(args))
+
+          val ((nodes, g), loss) = graph.run(init).value
+          require(nodes.size == 1)
+          val gradients = g.backProp(List(w, b), nodes.head)
+
+          (gradients("w").asInstanceOf[TVector].v, gradients("b").asInstanceOf[Scalar].v, loss.asInstanceOf[Scalar].v)
+        }
+
+      val wGradients = a.map(_._1)
+      val bGradients = a.map(_._2)
+      val losses = a.map(_._3)
+
+      val avgLoss = losses.toVector.sum / chk.size
+      println(s"avg loss: $avgLoss")
+
+      val avgBiasGradient = bGradients.toVector.sum / chk.size
+
+      val sumWeightsGradient = wGradients.toVector
+        .reduce[Vector[Double]] { case (v1, v2) => v1.zip(v2).map(t => t._1 + t._2) }
+
+      val avgWeightsGradient = sumWeightsGradient.map(_ / chk.size)
+
+      (
+        weights.zip(avgWeightsGradient).map { case (w, g) => w - learningRate * g },
+        bias - learningRate * avgBiasGradient
+      )
+  }
+
   val (trainedWeights, trainedBias) =
-  trainData.chunkN(batchSize).fold((initialWeights, initialBias))(trainSink).compile.toVector.unsafeRunSync()
-    .head
+    trainData.chunkN(batchSize).fold((initialWeights, initialBias))(trainSinkWithGraph).compile.toVector.unsafeRunSync()
+      .head
 
   // training error
   val prediction =
-    trainData.map{case (y, img) => {
+    trainData.map { case (y, img) => {
       val yHat = network.forwardProp(trainedWeights, trainedBias, img)
       val prediction = if (yHat > 0.5) 1 else 0
       y == prediction
-    }}.compile.toVector.unsafeRunSync()
+    }
+    }.compile.toVector.unsafeRunSync()
 
-  println(s"correct prediction: ${prediction.count(t => t)/prediction.size.toDouble}")
+  println(s"correct prediction: ${prediction.count(t => t) / prediction.size.toDouble}")
 
 
 }
--- core/src/org/jsun/scalax/nn/graph/Tensor.scala
+++ core/src/org/jsun/scalax/nn/graph/Tensor.scala
@@ -31,7 +31,23 @@ object Tensor {
     def f(n:Node):Tensor
   }
 
-  //  case class CrossProduct extends Op
+  case object CrossProduct extends BinaryOp {
+    override def f(n1: Node, n2: Node): Tensor = (n1.v, n2.v) match {
+      case (TVector(v1), TVector(v2)) => Scalar(
+        v1.zip(v2).map(t => t._1 * t._2).sum
+      )
+    }
+
+    override def bprop(inputs: List[Node], x: Node, g: Tensor): Tensor = {
+      require(inputs.size == 2)
+      val other = inputs.filter(_.name != x.name)
+      require(other.size == 1)
+
+      (g, other.head.v) match {
+        case (Scalar(v), TVector(t)) => TVector(t.map(_ * v))
+      }
+    }
+  }
 
   case object Add extends BinaryOp {
     override def bprop(inputs: List[Node], x: Node, g: Tensor) = g
@@ -70,7 +86,26 @@ object Tensor {
       }
     }
 
-  //  case class CrossEntropyLoss extends Op
+    case object CrossEntropyLoss extends BinaryOp{
+      override def f(n1: Node, n2: Node): Tensor = (n1.v, n2.v) match { // order is important
+        case (Scalar(z), Scalar(y)) => Scalar(math.log(1 + math.exp(z * (1-2*y))))
+      }
+
+      override def bprop(inputs: List[Node], x: Node, g: Tensor): Tensor = {
+        // only dL/dz
+        require(inputs.size == 2)
+        require(inputs.filterNot(_.name == x.name).size == 1)
+        val _y = inputs.filterNot(_.name == x.name).head
+
+        (_y.v, x.v) match {
+          case (Scalar(y), Scalar(z)) => {
+            val h = z * (1- 2 * y)
+            Scalar(1 / (1 + math.exp(h)) * math.exp(h) * (1 - 2 * y))
+          }
+        }
+
+      }
+    }
 
   case object Ident extends Op {
     override def bprop(inputs: List[Node], x: Node, g: Tensor) = g
